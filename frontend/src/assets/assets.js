59689a8bc843d5f188695ff77a94f750c6e7443b
Wael97EA <ahm8000ad90d@gmail.com>
2026-01-08 03:35:17 +0300

    commit02

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -72,11 +72,15 @@ import contact_img from "./contact_img.png";
 import razorpay_logo from "./razorpay_logo.png";
 import stripe_logo from "./stripe_logo.png";
 import cross_icon from "./cross_icon.png";
+import landing01 from "./landing01.jpg";
+import landing02 from "./landing02.avif";
 
 import logo_c from "./logo_c.png";
 import logo_t from "./logo_t.png";
 
 export const assets = {
+  landing01,
+  landing02,
   logo_c,
   logo_t,
   logo,
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,29 +1,28 @@
-import React from 'react'
-import { assets } from '../assets/assets.js'
+import React from "react";
+import { assets } from "../assets/assets.js";
 const Hero = () => {
-    return (
-        <div className='flex flex-col sm:flex-row border border-gray-400'>
-            {/* Hero left side */}
-            <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
-                <div className='text-[#414141]'>
-                    <div className='flex items-center gap-2'>
-                        <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
-                        <p className='font-medium text-sm md:text-base'>Our Bestsellers</p>
-                    </div>
-                    <h1 className='cinzel-400 text-3xl sm:py-3 lg:text-5x1 leading-relaxed'>Latest Arrivals</h1>
-                    <div className='flex items-center gap-2'>
-                        <p className='font-semibold md:text-base text-sm'>Shop Now</p>
-                        <p className='w-8 md:w-11 h-[1px] bg-[#414141]'></p>
-                    </div>
-                </div>
-            </div>
-
-            {/* Hero right side */}
-
-            <img src={assets.hero_img} className='w-full sm:w-1/2' alt="" />
-
+  return (
+    <div className="flex flex-col sm:flex-row border border-gray-400">
+      {/* Hero left side */}
+      <div className="w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0">
+        <div className="text-[#414141]">
+          <div className="flex items-center gap-2">
+            <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
+            <p className="font-medium text-sm md:text-base">Our Bestsellers</p>
+          </div>
+          <h1 className="cinzel-400 text-4xl sm:py-3 lg:text-5xl leading-relaxed">Latest Arrivals</h1>
+          <div className="flex items-center gap-2">
+            <p className="font-semibold md:text-base text-sm">Shop Now</p>
+            <p className="w-8 md:w-11 h-[1px] bg-[#414141]"></p>
+          </div>
         </div>
-    )
-}
+      </div>
 
-export default Hero
+      {/* Hero right side */}
+
+      <img src={assets.landing02} className="w-full sm:w-1/2" alt="" />
+    </div>
+  );
+};
+
+export default Hero;
